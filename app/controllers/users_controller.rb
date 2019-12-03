2c257d1244d96bfea54b19a9916f20fa02a6ce81
AnBinh <thanhbinhbk888@gmail.com>
2019-12-03 14:46:43 +0700

    Log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:info] = t ".info"
       redirect_to root_path
     else
