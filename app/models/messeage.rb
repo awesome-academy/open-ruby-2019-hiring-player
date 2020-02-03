7df479ed9ab1eeeb6df48b4c1001a9d53303b7a9
AnBinh <thanhbinhbk888@gmail.com>
2020-02-03 08:16:28 +0700

    Messenger

--- app/models/messeage.rb
+++
@@ -1,3 +0,0 @@
-class Messeage < ApplicationRecord
-  has_many :sender_recipient, as: :send_recieve
-end
--- db/migrate/20191126091025_create_messeages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMesseages < ActiveRecord::Migration[6.0]
-  def change
-    create_table :messeages do |t|
-      t.string :messeage
-
-      t.timestamps
-    end
-  end
-end
